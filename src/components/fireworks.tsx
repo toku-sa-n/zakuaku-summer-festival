77120308a2c04db41b02c3fe5614ce0d9c82f356
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-03 20:47:52 +0900

    Draw a particle

--- src/components/fireworks.tsx
+++ src/components/fireworks.tsx
@@ -9,9 +9,13 @@ export default function Fireworks() {
         { ssr: false }
     );
 
+    let particle: Particle;
+
     function setup(p5: p5Types, canvasParentRef: Element) {
         p5.createCanvas(p5.windowWidth, p5.windowHeight);
         p5.frameRate(fps);
+
+        particle = new Particle(p5.width / 2, p5.height / 2);
     }
 
     function windowResized(p5: p5Types) {
@@ -20,7 +24,23 @@ export default function Fireworks() {
 
     function draw(p5: p5Types) {
         p5.clear();
+
+        particle.draw(p5);
     }
 
     return <Sketch setup={setup} windowResized={windowResized} draw={draw} />;
+}
+
+class Particle {
+    private x: number;
+    private y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(p5: p5Types) {
+        p5.ellipse(this.x, this.y, 10, 10);
+    }
 }
