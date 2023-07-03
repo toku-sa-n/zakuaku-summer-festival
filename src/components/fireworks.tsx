4cb3d9264146f8f1e6164d87a1694a37f9c3fa9c
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-03 20:44:47 +0900

    Set FPS

--- src/components/fireworks.tsx
+++ src/components/fireworks.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import p5Types from "p5";
 
+const fps: number = 60 as const;
+
 export default function Fireworks() {
     const Sketch = dynamic(
         () => import("react-p5").then((mod) => mod.default),
@@ -9,13 +11,16 @@ export default function Fireworks() {
 
     function setup(p5: p5Types, canvasParentRef: Element) {
         p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        p5.frameRate(fps);
     }
 
     function windowResized(p5: p5Types) {
         p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
     }
 
-    function draw(p5: p5Types) {}
+    function draw(p5: p5Types) {
+        p5.clear();
+    }
 
     return <Sketch setup={setup} windowResized={windowResized} draw={draw} />;
 }
