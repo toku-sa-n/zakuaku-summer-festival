b776325e35130b41e3df1d234e2ca7d07ecd0cde
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-11 21:39:20 +0900

    Fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    images: {
-        loader: "akamai",
-        path: "",
-    },
-    assetPrefix: "./",
+    assetPrefix: "/zakuaku-summer-festival",
 };
 
 module.exports = nextConfig;
