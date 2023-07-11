1306e83e3bec5891c3fb2c10377bf642974fba8a
Hiroki Tokunaga <tokusan441@gmail.com>
2023-07-11 21:35:49 +0900

    Fix images not rendered on GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    images: {
+        loader: "akamai",
+        path: "",
+    },
+    assetPrefix: "./",
 };
 
 module.exports = nextConfig;
